f2832ff5cdd82798bf406c15341bd78c446cf361
AiBrary8 <118710404+AiBrary8@users.noreply.github.com>
2024-11-13 16:26:01 +0000

    Added the test cases

--- calculator.py
+++ calculator.py
@@ -3,24 +3,63 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
+        neg = -1
+        if(a < 0):
+            neg = -neg
+            a = -a
+        if(b < 0):
+            neg = -neg
+            b = -b
+        for i in range(b):
             result = self.add(result, a)
+        if(neg == 1):
+            result = -result
         return result
 
     def divide(self, a, b):
+        if(b == 0):
+            return "Invalid denominator = 0"
         result = 0
-        while a > b:
+        neg = -1
+        if(a < 0):
+            neg = -neg
+            a = a - a - a
+        if(b < 0):
+            neg = -neg
+            b = b - b - b
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
+        if (neg == 1):
+            result = -result
         return result
     
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
+        if(b == 0):
+            return "Invalid b = 0"
+        if(a == 0):
+            return 0
+        swap = 0
+        neg = -1
+        if(a < 0):
+            swap+=1
+            a = -a
+        if(b < 0):
+            neg = 1
+            swap-=1
+            b = -b
+        while a >= b:
+            a = a - b
+        if(swap != 0):
+            a = a - b
+        if(swap == 0 and neg == 1):
+            a = -a
+        if(neg != 1 and a < 0):
+            a = -a
         return a
 
 # Example usage:
@@ -29,6 +68,6 @@ if __name__ == "__main__":
     print("This is a simple calculator class!")
     print("Example: addition: ", calc.add(1, 2))
     print("Example: subtraction: ", calc.subtract(4, 2))
-    print("Example: multiplication: ", calc.multiply(2, 3))
-    print("Example: division: ", calc.divide(10, 2))
-    print("Example: modulo: ", calc.modulo(10, 3))
+    print("Example: multiplication: ", calc.multiply(2, 5))
+    print("Example: division: ", calc.divide(10, -2))
+    print("Example: modulo: ", calc.modulo(0, 8))
--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,31 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(-1,-2),-3)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(4, -2), 6)
+        self.assertEqual(self.calc.subtract(4, 2), 2)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, -2), -8)
+        self.assertEqual(self.calc.multiply(4, 2), 8)
+        self.assertEqual(self.calc.multiply(-4, -2), 8)
+    
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, -2), -5)
+        self.assertEqual(self.calc.divide(-10, 2), -5)
+        self.assertEqual(self.calc.divide(-10, -2), 5)
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-10, 0), "Invalid denominator = 0")
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10, 8), 2)
+        self.assertEqual(self.calc.modulo(10, -8), -6)
+        self.assertEqual(self.calc.modulo(-10, 8), 6)
+        self.assertEqual(self.calc.modulo(-10, -8), -2)
+        self.assertEqual(self.calc.modulo(-10, 0), "Invalid b = 0")
+        self.assertEqual(self.calc.modulo(0, -8), 0)
 
     # Add the following test methods to the TestCalculator class:
 
